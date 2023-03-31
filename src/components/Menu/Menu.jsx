ecd0742e863361e58b6b056ee947374e9e2ab626
Rayane Marques <125264099+DevRayaneMarques@users.noreply.github.com>
2023-03-31 17:21:13 -0300

    Revert "implementação username no menu"

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,70 +1,97 @@
-import "./Menu.css";
-import { Container, Nav, Navbar } from "react-bootstrap";
-import logoIcon from "./../../assets/icons/livros.png";
-import { Link, useNavigate } from "react-router-dom";
-import { logout } from "../../firebase/auth";
-import { useState, useEffect, useContext } from "react";
-import { auth } from "../../firebase/config";
-import { AuthContext } from "../../contexts/AuthContext";
-
+import './Menu.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import logoIcon from './../../assets/icons/livros.png';
+import { Link, useNavigate } from 'react-router-dom';
+import { logout } from '../../firebase/auth';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { loginGoogle, loginEmailSenha } from '../../firebase/auth';
 
 export function Menu() {
   const navigate = useNavigate();
-  const [userDisplayName, setUserDisplayName] = useState("");
-  const usuarioLogado = useContext(AuthContext);
-
-
-  useEffect(() => {
-    // const unsubscribe = auth.onAuthStateChanged((user) => {
-    //   if (user) {
-    //     setUserDisplayName(userDisplayName);
-    //   } else {
-    //     setUserDisplayName("");
-    //   }
-    // });
-
-    // return unsubscribe;
-    setUserDisplayName(usuarioLogado.displayName)
-
-  }, []);
+  const [isMouseOverIcon, setIsMouseOverIcon] = useState(false);
 
   function onLogout() {
     logout().then(() => {
-      navigate("/login");
+      navigate('/login');
     });
   }
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <Navbar bg="success" variant="light" expand="lg">
       <Container fluid>
         <Navbar.Brand>
-          <Link to="/">
-            <img src={logoIcon} width="32" alt="Logo" />
-          </Link>
+          {
+            <Link to="/">
+              <img src={logoIcon} width="32" alt="Logo" />
+            </Link>
+          }
         </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse>
-          <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/">
-              Home
-            </Nav.Link>
-            <Nav.Link as={Link} to="/livros">
-              Livros
-            </Nav.Link>
-            <Nav.Link as={Link} to="/emprestimos">
-              Emprestimos
-            </Nav.Link>
-            <Nav.Link as={Link} to="/usuario">
-              {userDisplayName}
+
+        <Button
+          variant="dark"
+          className="d-lg-none menu-btn"
+          onClick={handleShow}
+        >
+          <span className="icon">
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </span>
+        </Button>
+
+        <Offcanvas
+          show={show}
+          onHide={handleClose}
+          responsive="lg"
+          style={{ backgroundColor: '#00000035' }}
+        >
+          <Offcanvas.Header
+            closeButton
+            closeVariant="white"
+            style={{ color: 'white' }}
+          >
+            <Offcanvas.Title style={{ marginRight: '200px' }}>
+              Menu
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="ms-auto navText my-nav">
+              <Nav.Link as={Link} to="/" className="navText">
+                Home
               </Nav.Link>
-              <Nav.Link as={Link} to="/usuario">
-                {userDisplayName ? userDisplayName : usuarioLogado.email.split("@")[0]}
-                </Nav.Link>
-            <Nav.Link onClick={onLogout}>
-              <i className="bi bi-box-arrow-right"></i>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+              <Nav.Link as={Link} to="/livros" className="navText">
+                Livros
+              </Nav.Link>
+
+              <Nav.Link as={Link} to="/emprestimos" className="navText">
+                Emprestimos
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Ajuda">
+                Ajuda
+              </Nav.Link>
+
+
+
+              <Nav.Link
+                onClick={onLogout}
+                className="navText"
+                onMouseOver={() => setIsMouseOverIcon(true)} // Altera o estado para indicar que o mouse está sobre o ícone
+                onMouseOut={() => setIsMouseOverIcon(false)} // Altera o estado para indicar que o mouse não está mais sobre o ícone
+              >
+                <i
+                  className="bi bi-box-arrow-right"
+                  style={{ color: isMouseOverIcon ? '#5ecfff' : 'white' }} // Verifica se o mouse está sobre o ícone e altera a cor de acordo
+                ></i>
+              </Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Offcanvas>
       </Container>
     </Navbar>
   );
