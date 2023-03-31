99180688d8d18292ed3a386717f478ae643b9801
DevRayaneMarques <boscolorayane@gmail.com>
2023-03-31 16:39:12 -0300

    alteração no menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -3,9 +3,30 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import logoIcon from "./../../assets/icons/livros.png";
 import { Link, useNavigate } from "react-router-dom";
 import { logout } from "../../firebase/auth";
+import { useState, useEffect, useContext } from "react";
+import { auth } from "../../firebase/config";
+import { AuthContext } from "../../contexts/AuthContext";
+
 
 export function Menu() {
   const navigate = useNavigate();
+  const [userDisplayName, setUserDisplayName] = useState("");
+  const usuarioLogado = useContext(AuthContext);
+
+
+  useEffect(() => {
+    // const unsubscribe = auth.onAuthStateChanged((user) => {
+    //   if (user) {
+    //     setUserDisplayName(userDisplayName);
+    //   } else {
+    //     setUserDisplayName("");
+    //   }
+    // });
+
+    // return unsubscribe;
+    setUserDisplayName(usuarioLogado.displayName)
+
+  }, []);
 
   function onLogout() {
     logout().then(() => {
@@ -33,6 +54,12 @@ export function Menu() {
             <Nav.Link as={Link} to="/emprestimos">
               Emprestimos
             </Nav.Link>
+            <Nav.Link as={Link} to="/usuario">
+              {userDisplayName}
+              </Nav.Link>
+              <Nav.Link as={Link} to="/usuario">
+                {userDisplayName ? userDisplayName : usuarioLogado.email.split("@")[0]}
+                </Nav.Link>
             <Nav.Link onClick={onLogout}>
               <i className="bi bi-box-arrow-right"></i>
             </Nav.Link>
